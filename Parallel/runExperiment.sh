b2eed070eba2b38d4741736809c8f301b4a20ac2
XPPGX <qwerlegendary@gmail.com>
2023-11-11 02:23:11 +0800

    script for experiments

---
+++ Parallel/runExperiment.sh
@@ -0,0 +1,7 @@
+for i in {1..38}
+do
+    ./ECL-GC/a ../Dataset/dblp.txt
+    ./ECL-GC/a ../Dataset/amazon.txt
+    ./ECL-GC/a ../Dataset/LiveJournal.txt
+    ./ECL-GC/a ../Dataset/roadNet-CA_OK.txt
+done
